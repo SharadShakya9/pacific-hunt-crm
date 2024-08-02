41ed02bec3678a00c2621370a6580a0dab1ca2cd
SharadShakya9 <shakyasharad9@gmail.com>
2024-08-02 16:34:16 +0545

    Dashboard page modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import PageLogo from "/logo pacific 1.png";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-screen bg-light-2">
-      <Link href="/dashboard">
-        <p>Continue to Dashboard</p>
-      </Link>
+    <main className="flex min-h-screen w-screen items-center justify-center bg-dark-2">
+      <div className="flex flex-col gap-5">
+        <img src="/logo pacific 1.png" alt="" />
+
+        <Link href="/dashboard">
+          <p className="text-24-700 italic text-blue-1">
+            Continue to Dashboard {">>"}
+          </p>
+        </Link>
+      </div>
     </main>
   );
 }
